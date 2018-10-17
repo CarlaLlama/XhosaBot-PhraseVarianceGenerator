6b83b3486e35f4d46acc1b3fda63f5382e414d6c
Carla Wilby <carla@zelda.org.za>
2018-10-17 20:16:18 +0200

    Initialized Phrase Variation Generator

---
+++ faq_dataset.py
@@ -0,0 +1,77 @@
+
+# The original training samples.
+# Format: [Phrase, Label, Language code]
+
+labelled_phrases_list = [
+    # Phrase 1:
+    ["Kutheni umntwana wam engalali?", "lala", "xho"],
+
+    # Phrase 2:
+    ["Ndingambhalisa njani umntwana wam?", "bhala", "xho"],
+
+    # Phrase 3:
+    ["Impawu zokuba n dizokubeleka?", "zokubeleka", "xho"],
+
+    # Phrase 4:
+    ["Ndazi kanjani ukuba ndizokubeleka phambi kwexesha?", "phambi", "xho"],
+
+    # Phrase 5:
+    ["Ndine pre-eclampsia?", "pre-eclampsia", "xho"],
+
+    # Phrase 6:
+    ["Lide kangakanani ixesha lokubeleka?", "ixhesha", "xho"],
+
+    # Phrase 7:
+    ["Ndinesifo sasekuseni?", "morning_sickness", "xho"],
+
+    # Phrase 8:
+    ["Ndine UTI ngelixa ndikhulelwe", "uti", "xho"],
+
+    # Phrase 9:
+    ["I high-blood pressure ngelixa ukhulelwe", "high-blood", "xho"],
+
+    # Phrase 10:
+    ["Ndinesifo sokwasulelana ngesondo", "std", "xho"],
+
+    # Phrase 11:
+    ["Ndiyagula ngelixa ndikhulelwe", "ndiyagula", "xho"],
+
+    # Phrase 12:
+    ["Umntwana wam uyagula", "uyagula", "xho"],
+
+    # Phrase 13:
+    ["Ndineentlungu, ndingaya njani ekliniki?", "ekliniki", "xho"],
+
+    # Phrase 14:
+    ["Ndinemilenze edumbileyo", "dumbileyo", "xho"],
+
+    # Phrase 15:
+    ["Yintoni endimele ndondle ngayo umntwana wam?", "ndondle", "xho"],
+
+    # Phrase 16:
+    ["Ndiyopha ngexesha lokukhulelwa", "opha", "xho"],
+
+    # Phrase 17:
+    ["Ndinocinezelelo lwengqondo", "depressed", "xho"],
+
+    # Phrase 18:
+    ["Ndingasifumana njani isibonelelo somntwana wam?", "isibonelelo", "xho"],
+
+    # Phrase 19:
+    ["Ingaba umntwana wam uzobane HIV?", "hiv", "xho"],
+
+    # Phrase 20:
+    ["Ndingancancisa ndine HIV?", "hiv_cancisa", "xho"],
+
+    # Phrase 21:
+    ["Umntwana wam unorhawuzelelo owenziwa yi-nappy", "nappy_rash", "xho"],
+    # Phrase 22:
+    ["Umntwana wam akatyi", "akatyi", "xho"],
+
+    # Phrase 23:
+    ["Ndingabelana ngesondo xa dikhulelwe?", "ngesondo", "xho"],
+
+    # Phrase 24:
+    ["Ndingayenza I C-Section?", "c-section", "xho"],
+    ["Ndingayenza uqhaqho?", "c-section", "xho"],
+]
